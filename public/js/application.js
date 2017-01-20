7d384d1e012ef74c4cb1b5b6431cbdd9d7308867
Danny Maher & Dereck Miller <pair+D-Maher.dmiller0090@devbootcamp.com>
2017-01-20 11:45:28 -0600

    Introduce Google API into site

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,50 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+// $(document).ready(function() {
+  // var mapOptions = {
+  //   center: new google.maps.LatLng(37.0, -122.0),
+  //   zoom: 12,
+  //   mapTypeId: google.maps.mapTypeId.ROADMAP
+  // }
+  // new google.maps.Map($("#map"), mapOptions);
+  // console.log("HI!")
+  // userCoords = navigator.geolocation.getCurrentPosition(function(){});
+  // console.log(userCoords);
+  // debugger;
+// });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+// var getLocation = function() {
+//   var myLocation;
+//   myLocation =
+//   console.log("myLocation: " + myLocation);
+//   return myLocation;
+// }
+
+// function(location){
+//     return location;
+//   }
+
+function initialize(location){
+  console.log(location);
+  // debugger;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: location.coords.latitude, lng: location.coords.longitude},
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(location.coords.latitude, location.coords.longitude)
+  })
+  marker.setMap(map)
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+};
+
+$(document).ready(function(){
+  var options = {
+    enableHighAccuracy: true,
+    maximumAge: 0
+  }
+  navigator.geolocation.getCurrentPosition(initialize, error, options);
 });
